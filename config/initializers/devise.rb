570abb5ac82794d1d0f1cc51ccc366ecc0efa3af
Vitor <vitorguima@gmail.com>
2021-09-09 20:23:21 -0300

    change omniauth scopes to: channel:manage, channel:read, user:read

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :twitch, ENV['TWITCH_APP_ID'], ENV['TWITCH_APP_SECRET']
+  config.omniauth :twitch, ENV['TWITCH_APP_ID'], ENV['TWITCH_APP_SECRET'], scope: 'channel:manage:broadcast channel:read:stream_key user:read:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
