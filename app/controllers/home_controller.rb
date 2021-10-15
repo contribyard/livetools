4c69c460853d0ccbec0c8c251db05a122779df3b
Vitor <vitorguima@gmail.com>
2021-10-14 22:40:06 -0300

    change redirect after login to edit user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    redirect_to new_user_session_url unless current_user
+    url_to_redirect = if user_signed_in?
+      edit_user_registration_path
+    else
+      new_user_session_url
+    end
+    
+    redirect_to url_to_redirect
   end
 end
