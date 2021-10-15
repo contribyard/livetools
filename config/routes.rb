2e43c8860c3dfebf7bd7eeef29e36546fb42c114
Vitor <vitorguima@gmail.com>
2021-10-14 21:13:10 -0300

    inert home_controller and root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
   # post '/auth/twitch/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
 end
